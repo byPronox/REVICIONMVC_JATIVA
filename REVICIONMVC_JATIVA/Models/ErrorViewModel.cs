183dca3cd73598b2c8c7fe85664ec07f15385932
laboratorio <laboratorio@UPOAULA10306>
2024-10-10 08:22:08 -0500

    Agregar archivos de proyecto.

---
+++ REVICIONMVC_JATIVA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace REVICIONMVC_JATIVA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
